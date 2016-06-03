9acc1df909a0de334b3a7f4869cafab2bbcd7c41
Henning Steinke <henning85@gmail.com>
2016-06-03 12:17:36 +0200

    Initial commit

---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios, '8.0'
+use_frameworks!
+
+target 'Weather Routing' do
+  pod 'Mapbox-iOS-SDK', '~> 3.2.2'
+end
---
+++ Pods/Target Support Files/Pods-Weather Routing/Pods-Weather Routing-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_Weather_RoutingVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Weather_RoutingVersionString[];
+
