1ddbfb038f7c2f681d9fd1701584a12b26acd9ae
Henning Steinke <henning85@gmail.com>
2016-06-03 12:12:33 +0200

    Initial Commit

---
+++ Weather Routing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Weather Routing
+//
+//  Created by Henning Steinke on 03.06.16.
+//  Copyright © 2016 Henning Steinke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Weather RoutingTests/Weather_RoutingTests.swift
@@ -0,0 +1,36 @@
+//
+//  Weather_RoutingTests.swift
+//  Weather RoutingTests
+//
+//  Created by Henning Steinke on 03.06.16.
+//  Copyright © 2016 Henning Steinke. All rights reserved.
+//
+
+import XCTest
+@testable import Weather_Routing
+
+class Weather_RoutingTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
